9f9b002ba1c0b9bb00d9690367d021706e549c98
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2026-01-16 11:11:44 +0100

    add setup file

---
+++ setup.sh
@@ -0,0 +1,4 @@
+mkdir ./test-data
+
+git clone git@github.com:corazawaf/libinjection-go.git ./test-data/libinjection-go
+git clone git@github.com:libinjection/libinjection.git ./test-data/libinjection
